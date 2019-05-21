05507ffc7a21f12d048d1be08c456c9170f62f6c
sathya.murali <techsathyam@gmail.com>
2019-05-21 15:04:02 -0500

    Adding function call

--- queryOMDBAPI.groovy
+++ queryOMDBAPI.groovy
@@ -6,12 +6,14 @@ Purpose : This file queries OMDB API based on Movie Name
 
 def queryomdb() {
     def baseURL = 'http://www.omdbapi.com/'
-    def apikey = f4cba32c
+    def apikey = 'f4cba32c'
     println "Please input Movie Name to Query"
-    def movieName = System.in.readLine()
+    def movieName = System.in.newReader().readLine()
 
     println "Running Query"
     sh """
     curl ${baseURL}\\?t=${movieName}&apikey=${apikey}
     """  
 }
+
+queryomdb()
